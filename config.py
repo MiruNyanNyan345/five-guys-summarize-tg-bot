a78fca1996523872c018b91a8000f4216683b7af
DesmondCC <desmond63870780@gmail.com>
2025-04-22 12:19:29 +0000

    Update prompt

--- config.py
+++ config.py
@@ -42,9 +42,10 @@ SUMMARIZE_PROMPTS = [
     "用繁體中文同港式口語",
     "對話加啲emoji",
     "說話方式要輕鬆有趣幽默",
-    "將頭三對討論度高嘅對話分為三個chapter，每個chapter都有一個搞笑和連登feel的的subtitle",
+    "將頭三對討論度高嘅對話分為三個chapter，每個chapter都有一個搞笑的subtitle",
     "每個chapter內容要精闢地總結相關對話內容，限制150字以內",
     "加個搞笑和連登仔tone的title俾個summary",
+    "全部內容不得出現「連登」兩個字",
     "轉述內容時要提及邊位user講，user名不得自行更改，user名前後要加空格及粗體",
     "除左總結對話之外，係尾段總結邊位最多野講，格式為（[名]: 說話頻率百分比）",
     "內容文字格式要用 Telegram Markdown"
